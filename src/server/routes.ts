be4f67e7f52ba05a6b336d5e0a4cbd632bbc4ca2
Gabriel-Grechuk <gabrielgrechuk@outlook.com>
2024-10-05 16:48:39 -0300

    #5 - Create the elevation route

--- src/server/routes.ts
+++ src/server/routes.ts
@@ -1,8 +1,43 @@
 import express from "express";
+import { logRequest } from "../utils/logging";
+import axios from "axios";
 
 export function index(req: express.Request, res: express.Response) {
-  console.log("Request to", req.originalUrl);
-  console.log(req.ip);
-  console.log();
+  logRequest(req);
   res.send("<h1>Opa, bão</h1>");
+}
+
+export async function elevationByCoord(
+  req: express.Request,
+  res: express.Response,
+) {
+  try {
+    logRequest(req);
+
+    const lat = req.query?.lat;
+    const long = req.query?.long;
+
+    if (!lat || !long) {
+      res.send(
+        `<h1>Deu errado pai, argumentos inválidos</h1><p>Args:</p><p>${JSON.stringify(req.query)}</p>`,
+      );
+      return;
+    }
+
+    const queryUrl = `https://maps.googleapis.com/maps/api/elevation/json?locations=${
+      lat
+    }%2C${long}&key=${process.env.GOOGLE_API_KEY}`;
+
+    const googleRes = await axios.get(queryUrl);
+
+    const response = {
+      elevation: googleRes.data.results[0].elevation,
+    };
+
+    res.send(response);
+  } catch (error) {
+    res.send(
+      `<h1>Deu errado pai, argumentos inválidos</h1><p>Args:</p><p>${JSON.stringify(req.query)}</p>`,
+    );
+  }
 }
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,10 +1,11 @@
-import express from 'express';
+import express from "express";
 
-import * as routes from './routes';
+import * as routes from "./routes";
 
 const app = express();
 
-app.get('/', routes.index);
+app.get("/", routes.index);
+app.get("/elevation", routes.elevationByCoord);
 
 export default {
   app,
